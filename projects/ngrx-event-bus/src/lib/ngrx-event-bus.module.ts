f51d4a266e523d802dd0727bf9d3cd9364845c50
Abhishek Joshi <abhishek2185@live.in>
2021-06-16 10:21:59 +0530

    base library

---
+++ projects/ngrx-event-bus/src/lib/ngrx-event-bus.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgrxEventBusComponent } from './ngrx-event-bus.component';
+
+
+
+@NgModule({
+  declarations: [NgrxEventBusComponent],
+  imports: [
+  ],
+  exports: [NgrxEventBusComponent]
+})
+export class NgrxEventBusModule { }
---
+++ projects/ngrx-event-bus/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngrx-event-bus
+ */
+
+export * from './lib/ngrx-event-bus.service';
+export * from './lib/ngrx-event-bus.component';
+export * from './lib/ngrx-event-bus.module';
